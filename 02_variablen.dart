4b5c366a7961b717556f123b17517a8fbe72fd82
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-02-11 01:36:22 +0100

    Added variablen.

---
+++ 02_variablen.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int alter = 32; // Initialisierung
+
+  late int alter_2; // Deklaration
+
+  alter_2 = 44; // Zuweisung
+
+  print(alter);
+
+  print(alter_2);
+
+  alter_2 = 53;
+
+  print(alter_2);
+}
